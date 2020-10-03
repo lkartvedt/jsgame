17af5d96d9d017ec8d2b106424c15cb72230e0b7
cwoll <ctwollaston@mymail.mines.edu>
2020-10-02 19:12:58 -0600

    Added diver to canvas.

--- cavediver.js
+++ cavediver.js
@@ -1,3 +1,12 @@
+//load image onto canvas
+const canvas = document.getElementById("mycanvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = "images/diver.jpg";
+img.onload = () =>{
+	ctx.drawImage(img, 0, 0);
+}
+
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
@@ -22,3 +31,35 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+//Make diver image move
+/*function move() {
+	document.onKeyAndMove;
+	var diver;
+	var posUp = 10;
+	var posDown = 10;
+	}
+	//function to make diver go up if up arrow pressed
+	function moveUp() {
+		alert("moved up");
+		posUp = posUp + 5px;
+	}
+	function moveDown() {
+		alert("moved down");
+		posDown = posDown + 5px;
+	}
+	//function to move image
+	function getKeyAndMove(b) {
+		var key_code = b.which||b.keyCode;
+		switch(key_code) {
+			//up arrow key
+			case 38:
+				moveUp();
+				break;
+			//down arrow key
+			case 40:
+				moveDown();
+				break;
+		}
+	}
+}*/
