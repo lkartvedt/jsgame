f9abaf1e7f58f814dcdf304c814528bae6ea5ce9
cwoll <ctwollaston@mymail.mines.edu>
2020-10-09 19:48:09 -0600

    Added up and down movement to diver image in canvas. Corrected spelling of name in index.html.

--- cavediver.js
+++ cavediver.js
@@ -1,10 +1,12 @@
+var x = 0;
+var y = 0;
 //load image onto canvas
 const canvas = document.getElementById("mycanvas");
 const ctx = canvas.getContext("2d");
 const img = new Image();
 img.src = "images/diver.jpg";
 img.onload = () =>{
-	ctx.drawImage(img, 0, 0);
+	ctx.drawImage(img, x, y);
 }
 
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
@@ -32,34 +34,36 @@ function switchTheme(e) {
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-//Make diver image move
-/*function move() {
-	document.onKeyAndMove;
-	var diver;
-	var posUp = 10;
-	var posDown = 10;
+document.addEventListener('keydown', checkKeyPressed, true);
+
+
+function up() {
+	y = y - 10;
+	ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
+	ctx.drawImage(img, x, y);
+}
+function down() {
+	y = y + 10;
+	ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
+	ctx.drawImage(img, x, y);
+}
+
+
+
+//write function for checkKeyPressed
+function checkKeyPressed(event){
+	//alert("checks key pressed");
+	switch(event.keyCode) {
+		//up key pressed 38
+		case 38:
+			//alert("up");
+			up();
+			break;
+		//down key pressed 40
+		case 40:
+			//alert("down");
+			down();
+			break;		
 	}
-	//function to make diver go up if up arrow pressed
-	function moveUp() {
-		alert("moved up");
-		posUp = posUp + 5px;
-	}
-	function moveDown() {
-		alert("moved down");
-		posDown = posDown + 5px;
-	}
-	//function to move image
-	function getKeyAndMove(b) {
-		var key_code = b.which||b.keyCode;
-		switch(key_code) {
-			//up arrow key
-			case 38:
-				moveUp();
-				break;
-			//down arrow key
-			case 40:
-				moveDown();
-				break;
-		}
-	}
-}*/
+}
+
